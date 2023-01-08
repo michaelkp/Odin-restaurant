fc3e90775161aa8e5f4b811933740dbbd00259df
Michael <michaelk99@gmail.com>
2023-01-08 00:43:08 -0800

    Setup Webpack and HtmlWebpackPlugin.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 console.log('test');
+
+import './style.css'
+
+const content = document.querySelector('.content')
+
+const header = document.createElement('header')
+    header.textContent = 'Header'
+    content.appendChild(header)
+
+const main = document.createElement('main')
+    content.appendChild(main)
+
+const nav = document.createElement('ul')
+    header.appendChild(nav)
+
+let navList = ['Home', 'Menu', 'About']
+navList.forEach((item) => {
+    const navListItem = document.createElement('li')
+        navListItem.classList.add('nav-li')
+        navListItem.id = item
+        navListItem.textContent = item
+        nav.appendChild(navListItem)
+        console.log(item);
+
+        navListItem.addEventListener('pointerup', (item) => {
+            console.log('event test ------------');
+            if(navListItem.id === 'Home') console.log('Home');
+            if(navListItem.id === 'Menu') console.log('Menu');
+            if(navListItem.id === 'About') console.log('About');
+        })
+})
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        home: './src/home.js',
+        about: './src/about.js',
+        menu: './src/menu.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Odin Coffee'
+        }),
+    ],
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     },
 }
